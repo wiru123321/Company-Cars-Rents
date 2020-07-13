7265e898449cf3317019f402d2fe6c8a21760beb
wiru123321 <wojtekmaj2@o2.pl>
2020-07-13 13:27:29 +0200

    Added route.

--- frontend/car-rental-app/src/App.js
+++ frontend/car-rental-app/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
+import { Route, Switch, HashRouter } from "react-router-dom";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <HashRouter basename="/">
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/userPage" component={Navbar} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
