7e9f2796559cc15a3939999b04a6aa2083132ecd
wiru123321 <wojtekmaj2@o2.pl>
2020-07-13 13:09:31 +0200

    Add Navbar.

--- frontend/car-rental-app/src/App.js
+++ frontend/car-rental-app/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Navbar from "./components/navbar/navbar";
 
 function App() {
@@ -7,16 +6,6 @@ function App() {
     <div>
       <Navbar />
     </div>
-=======
-import Login from "./components/login/Login";
-import { Container, Input } from "./styles/styles.style";
-function App() {
-  return (
-    <Container bgr="#22d1ee" col height="100vh">
-      <Login />
-    </Container>
->>>>>>> 3daef1d594d208002dfc86dc23f48d2b62dc450f
   );
 }
-
 export default App;
