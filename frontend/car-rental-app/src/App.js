8027359d8b9f07bbfd628da284d0d4af32024f03
wiru123321 <wojtekmaj2@o2.pl>
2020-07-13 12:38:23 +0200

    Add navbar

--- frontend/car-rental-app/src/App.js
+++ frontend/car-rental-app/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
