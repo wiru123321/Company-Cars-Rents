ac197d40132f2c62e8bf27716cb33cf9174da60c
wiru123321 <wojtekmaj2@o2.pl>
2020-07-24 14:40:12 +0200

    Changed in UserNavbar.

--- frontend/car-rental-app/src/components/userPage/UserPage.jsx
+++ frontend/car-rental-app/src/components/userPage/UserPage.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
-import ShowCars from "./showCars/ShowCars";
 import YourReservation from "./yourReservation/YourReservation";
-import ReportBug from "./reportBug/ReportBug";
 import UserNavbar from "../userNavbar/UserNavbar";
 import ShowAllCars from "./showAllCars/ShowAllCars";
 import Reservation from "./reservation/Reservation";
@@ -13,9 +11,7 @@ const UserPage = () => {
       <UserNavbar />
       <HashRouter basename="/userPage">
         <Switch>
-          <Route path="/showAllCars" component={ShowCars} />
           <Route path="/yourReservation" component={YourReservation} />
-          <Route path="/reportBug" component={ReportBug} />
           <Route path="/showAllCars" component={ShowAllCars} />
           <Route path="/reservation" component={Reservation} />
         </Switch>
