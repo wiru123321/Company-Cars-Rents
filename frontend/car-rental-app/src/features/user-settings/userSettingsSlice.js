726b582a4f2f80ec6a79c63daf6718da009fc0be
KamilSusek <dumacataloni@gmail.com>
2020-08-07 11:42:07 +0200

    User info update form gives information about update result.

--- frontend/car-rental-app/src/features/user-settings/userSettingsSlice.js
+++ frontend/car-rental-app/src/features/user-settings/userSettingsSlice.js
@@ -9,6 +9,9 @@ const initialState = {
   newPassword: "",
   newEmail: "",
   newPhoneNumber: "",
+  successMessage: "",
+  errorMessage: "",
+  showSubmitInfo: false,
 };
 
 export const userSettingsSlice = createSlice({
@@ -18,7 +21,6 @@ export const userSettingsSlice = createSlice({
     toggleForm: (state, action) => {
       state.changeFormId = action.payload;
     },
-
     setPassword: (state, action) => {
       state.password = action.payload;
     },
@@ -31,6 +33,26 @@ export const userSettingsSlice = createSlice({
     setNewPhoneNumber: (state, action) => {
       state.newPhoneNumber = action.payload;
     },
+    setSubmitResult: (state, action) => {
+      state.showSubmitInfo = action.payload;
+    },
+    setSuccessMessage: (state, action) => {
+      state.successMessage = action.payload;
+    },
+    setErrorMessage: (state, action) => {
+      state.errorMessage = action.payload;
+    },
+    resetInputFields: (state) => {
+      state.password = "";
+      state.newPassword = "";
+      state.newEmail = "";
+      state.newPhoneNumber = "";
+    },
+    resetErrorMessage: (state) => {
+      state.successMessage = "";
+      state.errorMessage = "";
+      state.showSubmitInfo = false;
+    },
   },
 });
 
@@ -40,6 +62,11 @@ export const {
   setNewPassword,
   setNewEmail,
   setNewPhoneNumber,
+  resetFields,
+  resetErrorMessage,
+  setSuccessMessage,
+  setErrorMessage,
+  setSubmitResult,
 } = userSettingsSlice.actions;
 
 export const selectAll = (state) => state.userSettings;
@@ -53,9 +80,16 @@ export const updateEmail = (editAccount) => async (dispatch) => {
         Authorization: "Bearer " + localStorage.getItem("token"),
       },
     });
-    console.log(response);
+    dispatch(setSubmitResult(true));
+    dispatch(setSuccessMessage("Email added successfully."));
   } catch (error) {
-    console.log(error);
+    if (error.response.status === 406) {
+      dispatch(setSubmitResult(false));
+      dispatch(setErrorMessage("Password is invalid."));
+    } else {
+      dispatch(setSubmitResult(false));
+      dispatch(setErrorMessage("Failed to update email."));
+    }
   }
 };
 
@@ -85,7 +119,8 @@ export const updatePassword = (editAccount) => async (dispatch) => {
     );
     console.log(response);
   } catch (error) {
-    console.log(error);
+    if (error.code === 406) console.log("bad password");
+    else console.log(JSON.stringify(error));
   }
 };
 
